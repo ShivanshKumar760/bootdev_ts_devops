55d48a9e62443c6efb607336cb9b18660eda25f9
Shivansh Kumar <104713531+ShivanshKumar760@users.noreply.github.com>
2026-08-20 04:13:38 +0530

    drizzle orm schema.ts added

--- 02.Build/src/config.ts
+++ 02.Build/src/config.ts
@@ -1,10 +1,24 @@
+import process from "node:process";
+
+// Load the environment variables from the .env file
+process.loadEnvFile();
+
+// Helper function to assert that the environment variable is present
+function envOrThrow(key: string): string {
+  const value = process.env[key];
+  if (!value) {
+    throw new Error(`Missing required environment variable: ${key}`);
+  }
+  return value;
+}
+
 export type APIConfig = {
   fileserverHits: number;
+  dbURL: string; // Typed as a string
 };
 
 // Create and export the stateful config object
 export const config: APIConfig = {
-  fileserverHits: 0
+  fileserverHits: 0,
+  dbURL: envOrThrow("DB_URL")
 };
-
-
