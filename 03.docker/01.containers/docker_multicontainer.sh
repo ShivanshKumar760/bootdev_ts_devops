392204eb9de20b0a5747f8a0556a8bc24cec6a06
Shivansh Kumar <104713531+ShivanshKumar760@users.noreply.github.com>
2026-08-25 04:26:52 +0530

    docker init

---
+++ 03.docker/01.containers/docker_multicontainer.sh
@@ -0,0 +1,5 @@
+docker run -d -p 8965:80 docker/getting-started
+docker run -d -p 8966:80 docker/getting-started
+docker run -d -p 8967:80 docker/getting-started
+docker run -d -p 8968:80 docker/getting-started
+docker run -d -p 8969:80 docker/getting-started
